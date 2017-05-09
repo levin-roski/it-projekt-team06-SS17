1e9137c9798cf500cc14785d52d75bf2eae13d27
theresa-vocke <theresa.vocke@outlook.de>
2017-05-09 17:13:57 +0200

    OrganisationMapper erstellen.

---
+++ src/de/worketplace/team06/server/db/OrganisationMapper.java
@@ -0,0 +1,78 @@
+package de.worketplace.team06.server.db;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.*;
+
+import de.worketplace.team06.shared.bo.*;
+
+public class OrganisationMapper {
+	
+	/**
+     * Default constructor
+     */
+	private static OrganisationMapper teamMapper = null;
+	 /**
+	   * Geschützter Konstruktor - verhindert die Möglichkeit, mit <code>new</code>
+	   * neue Instanzen dieser Klasse zu erzeugen.
+	   */
+	protected OrganisationMapper(){
+		
+	}
+	
+	public static OrganisationMapper organisationMapper(){
+		if (organisationMapper == null){
+			organisationMapper = new OrganisationMapper();
+		}
+		return organisationMapper; 
+	}
+    /**
+     * @param orgaUnit 
+     * @return
+     */
+    public Organisation insert (Organisation o) {
+        // TODO implement here
+    	Connection con = DBConnection.connection();
+    	
+    	try{
+    		Statement stmt = con.createStatement();
+    		
+    		}
+        return null;
+    }
+
+    /**
+     * @param orgaUnit 
+     * @return
+     */
+    public Organisation update(Organisation o) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param orgaUnit
+     */
+    public Organisation findById (int id){
+    	
+    }
+    
+    
+    public void delete(Organisation o) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public Vector<Organisaoion> findAll() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param orgaUnit 
+     * @return
+     */
+
+}
