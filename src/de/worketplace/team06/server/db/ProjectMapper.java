b363fd0e53edd4c0f9805c37edc6a5c2688395ee
theresa-vocke <theresa.vocke@outlook.de>
2017-05-24 13:28:22 +0200

    # WARNING: head commit changed in the mean Merge branch 'TheresaVocke'

--- src/de/worketplace/team06/server/db/ProjectMapper.java
+++ src/de/worketplace/team06/server/db/ProjectMapper.java
@@ -1,8 +1,13 @@
 package de.worketplace.team06.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 import de.worketplace.team06.shared.bo.Project;
+import de.worketplace.team06.shared.bo.Team;
 
 /**
  * 
@@ -24,38 +29,129 @@ public class ProjectMapper {
 		}
 		return projectMapper; 
 	}
-
-    /**
-     * @param project 
-     * @return
-     */
-    public Project insert(Project project) {
-        // TODO implement here
-        return null;
+   
+    public Project findById (int id){
+    	Connection con = DBConnection.connection();
+    	
+    	try{
+    		Statement stmt= con.createStatement();
+    		ResultSet rs = stmt.executeQuery("SELECT id, created FROM Team " + "WHERE id= " + id);
+    		
+    		if (rs.next()) {
+    			Project proj = new Project();
+    			proj.setID(rs.getInt("id"));
+    			proj.setTitle(rs.getString("Title"));
+    			proj.setDescription(rs.getString("description"));
+    			proj.setProjectLeaderID(rs.getInt("projectLeaderID"));
+    			proj.setProjectOwnerID(rs.getInt("projectOwnerID"));
+    			proj.setStartDate(rs.getDate("startDate"));
+    			proj.setEndDate(rs.getDate("endDate"));
+    			proj.setCreated(rs.getTimestamp("created"));
+    		}	
+    	}
+    	catch (SQLExpetion e){
+    		e.printStackTrace();
+    	}
+    	return rs;
     }
-
-    /**
-     * @param project 
-     * @return
-     */
-    public Project update(Project project) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param project
-     */
-    public void delete(Project project) {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
+  
     public Vector<Project> findAll() {
-        // TODO implement here
-        return null;
+        Connection con = DBConnection.connection();
+        Vector<Project> result = new Vector<Project>();
+        
+        try{
+        	Statement stmt = con.createStatement();
+        	
+        	ResultSet rs = stmt.executeQuery("SELECT id, title, description, "
+        			+ "projectLeaderID, projectOwnerID, startDate, endDate, created "
+        	+ "FROM Team ");
+        	
+        	while (rs.next()){
+        		Project proj = new Project();
+        		proj.setID(rs.getInt("id"));
+        		proj.setTitle(rs.getString("title"));
+        		proj.setDescription(rs.getString("description"));
+        		proj.setProjectLeaderID(rs.getInt("projectLeaderID"));
+        		proj.setProjectOwnerID(rs.getInt("projectOwnerID"));
+        		proj.setStartDate(rs.getDate("startDate"));
+        		proj.setEndDate(rs.getDate("endDate"));
+        		proj.setCreated(rs.getTimestamp("created"));
+        		
+        		result.addElement(proj);
+        	}
+        }
+        catch (SQLException e){
+        	e.printStackTrace();
+        }
+        return result ;
+    }
+    
+    
+    
+    public Project insert (Project proj) {
+        Connection con = DBConnection.connection();
+        
+        try {
+        	Statement stmt = con.createStatement();
+        	
+        	ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid " + "FROM project ");
+        	
+        	if (rs.next()){
+        	proj.setID(rs.getInt("maxid") + 1);
+        	stmt = con.createStatement();
+        	
+        	stmt.executeUpdate("INSERT INTO team (id, title, description, projectLeaderID, projectOwnerID, startDate, endDate) " 
+        	+ "VALUES (" 
+        	+ proj.getID() + ", " 
+        	+ proj.getTitle() + "','" 
+        	+ proj.getDescription() + "','"
+        	+ proj.getProjectLeaderID() + "','"
+        	+ proj.getProjectOwnerID() + "','"
+        	+ proj.getStartDate() + "','"
+        	+ proj.getEndDate() + "','"
+        	+ "')");
+        	}
+        }
+       catch (SQLExpetion e){
+    	e.printStackTrace();
+    }
+    
+    return proj;
+    } 
+    
+    public Project update(Project proj) {
+        Connection con = DBConnection.connection();
+        
+        try{
+        	Statement stmt = con.createStatement();
+        	
+        	stmt.executeUpdate("UPDATE project " 
+        	+ "SET title=\"" + proj.getTitle() + "\", " 
+        	+ "SET description=\"" + proj.getDescription() + "\", "
+        	+ "SET projectLeaderID=\"" + proj.getProjectLeaderID() + "\", "
+        	+ "SET projectOwnerID=\"" + proj.getProjectOwnerID() + "\", "
+        	+ "SET startDate=\"" + proj.getStartDate() + "\", "
+        	+ "SET endDate=\"" + proj.getEndDate() + "\", "
+        	+ "WHERE id=" + proj.getID());
+        }
+        
+        catch (SQLException e){
+        	e.printStackTrace();
+        } 
+        return proj;
+    }
+
+    public void delete(Project proj) {
+        Connection con = DBConnection.connection();
+        
+        try {
+        	Statement stmt = con.createStatement();
+        	
+        	stmt.executeUpdate("DELETE FROM project " + "WHERE id=" + proj.getID());
+        }
+        catch (SQLException e){
+        	e.printStackTrace();
+        }
     }
 
 	
