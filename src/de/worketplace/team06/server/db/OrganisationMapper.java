a5720198ae19a073310ea6c89e2deb0388b22426
theresa-vocke <theresa.vocke@outlook.de>
2017-06-13 15:36:46 +0200

    OrganisationMapper Kommentare hinzufügen.

--- src/de/worketplace/team06/server/db/OrganisationMapper.java
+++ src/de/worketplace/team06/server/db/OrganisationMapper.java
@@ -11,18 +11,40 @@ import de.worketplace.team06.shared.bo.*;
 public class OrganisationMapper {
 	
 	/**
-     * Default constructor
+     * Die Klasse OrganisationMapper wird nur einmal instantiiert. Man spricht hierbei
+     * von einem sogenannten <b>Singleton</b>.
+     * <p>
+     * Diese Variable ist durch den Bezeichner <code>static</code> nur einmal für
+     * sämtliche eventuellen Instanzen dieser Klasse vorhanden. Sie speichert die
+     * einzige Instanz dieser Klasse.
+     * 
+     * @author Thies
+     * @author Theresa
      */
+
 	private static OrganisationMapper organisationMapper = null;
+	
 	 /**
-	   * Gesch�tzter Konstruktor - verhindert die M�glichkeit, mit <code>new</code>
 	   * Geschuetzter Konstruktor - verhindert die Moeglichkeit, mit <code>new</code>
 	   * neue Instanzen dieser Klasse zu erzeugen.
+	   * 
+	   * @author Thies 
 	   */
 	
 	protected OrganisationMapper(){
 		
 	}
+	
+	/**
+	 * Diese Methode stellt sicher, dass die Singleton-Eigenschaft gegeben ist. Sie sorgt
+	 * dafür, dass nur eine einzige Instanz der OrganisationMapper-Klasse existiert. 
+	 * OrganisationMapper sollte nicht über den New-Operator, sondern über den 
+	 * Aufruf dieser statischen Methode.
+	 * 
+	 * @return OrganisationMapper
+	 * @author Thies
+	 * @author Theresa
+	 */
 	
 	public static OrganisationMapper organisationMapper(){
 		if (organisationMapper == null){
@@ -30,10 +52,16 @@ public class OrganisationMapper {
 		}
 		return organisationMapper; 
 	}
-
-    
 	
     /**
+	 * Einfuegen eines Organisation-Objektes in die Datenbank. Dabei wird auch der Primaerschluessel
+	 * des uebergebenen Objektes geprueft und ggf. berichtigt.
+	 * 
+	 * @param orgaUnit
+	 * @return Organisation
+	 * @throws  
+	 * @author Thies 
+	 * @author Theresa
      * @param orgaUnit 
      * @return
      */
@@ -44,6 +72,11 @@ public class OrganisationMapper {
 		try {
 			Statement stmt = con.createStatement();
 			
+			/** 
+			 * Abfragen des hoechsten Primaerschluesselwertes, die aktuelle ID 
+			 *wird dann um 1 erhoet und an an o vergeben
+			*/
+			
 			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid " + "FROM orgaunit ");
 			
 			if (rs.next()) {
@@ -52,6 +85,10 @@ public class OrganisationMapper {
 		
 				con.setAutoCommit(false);
 				stmt = con.createStatement();
+				
+				/**
+				 * Einfuegeoption, damit das neue Organisation-Tupel in die Datenbank eingefuegt werden kann
+				 */
 				stmt.executeUpdate("INSERT INTO orgaunit (id, created, googleID, description, type) " + "VALUES (" + o.getID() + ",'" + o.getCreated() + "','" + o.getGoogleID() +  "','" + o.getDescription() +  "','" + o.getType() + "')");
 				stmt.executeUpdate("INSERT INTO organisation (id, created, name, street, zipcode, city) " + "VALUES (" + o.getID() + ",'" + o.getCreated() + "','" + o.getName() + "','" + o.getStreet() + "'," + o.getZipcode() + ",'" + o.getCity() + "')");
 				con.commit();
@@ -74,8 +111,11 @@ public class OrganisationMapper {
 		return o;
     }
     
-	/**
-	 * Auslesen einer Organisation mithilfe einer GoogleID.
+    /**
+	 * Diese Methode findet ein Organisation-Objekt, anhand der übergebenen Google-ID 
+	 * 
+	 * @param googleID 
+	 * @return Organisation-Objekt 
 	 */
 	public Organisation findByGoogleID(String googleID) {
 		Connection con = DBConnection.connection();
@@ -107,9 +147,11 @@ public class OrganisationMapper {
 		return null;
 	}	
 
-    /**
+	/**
+     * Methode ermoeglicht, dass ein Organisation-Objekt in der Datenbank aktualisiert werden kann.
+     * 
      * @param orgaUnit 
-     * @return
+     * @return Organisation
      */
 	public void update(Organisation o) {
 		Connection con = DBConnection.connection();
@@ -134,7 +176,8 @@ public class OrganisationMapper {
 	}
     
     /**
-     * Löschen einer Organisation aus der Datenbank.
+     * Löschen eines Organisations-Objektes aus der Datenbank.
+     * 
      * @param orga
      */
     public void delete(Organisation o) {
@@ -149,6 +192,13 @@ public class OrganisationMapper {
 	    }
 	}
     
+    /**
+	 * Suchen einer Organisation mit vorgegebener OrganisationID. Durch die Eindeutigkeit der ID
+	 * wird genau ein Objekt zurück gegeben. 
+	 * 
+	 * @param ouid
+	 * @return Organisation-Objekt, das der übergebenen ID entspricht
+	 */
     public Organisation findByID(int ouid) {
     	Connection con = DBConnection.connection();
 		
@@ -156,6 +206,10 @@ public class OrganisationMapper {
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM orgaunit INNER JOIN organisation ON orgaunit.id = organisation.id WHERE orgaunit.id = " + ouid);		
 			
+			/**
+			 * es kann maximal nur ein Tupel zurueckgegeben werden, da ID Primaerschluessel ist, 
+			 * dann wird geprueft, ob für diese ID ein Tupel in der DB vorhanden ist
+			 */
 			if (rs.next()) {
 				Organisation o = new Organisation();
 				o.setID(rs.getInt("id"));
