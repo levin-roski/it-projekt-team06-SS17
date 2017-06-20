ff40ab17cefee04c375638c255bdd5ca729aa980
tobias-mueller1 <me@towbie.de>
2017-06-20 17:48:28 +0200

    ReportGenerator.java + Async anlegen und daraus die Impl erstellen

---
+++ src/de/worketplace/team06/shared/ReportGenerator.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.worketplace.team06.shared;
+
+import java.util.*;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import de.worketplace.team06.client.WindowAlertException;
+import de.worketplace.team06.shared.bo.*;
+
+/**
+ * @author Toby
+ *
+ */
+@RemoteServiceRelativePath("worketplaceAdmin")
+public interface ReportGenerator extends RemoteService {
+
+}
---
+++ src/de/worketplace/team06/shared/ReportGeneratorAsync.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared;
+
+import java.util.Date;
+import java.util.Vector;
+import de.worketplace.team06.shared.bo.*;
+
+/**
+ * Das asynchrone Gegenstück des Interface {@link ReportGenerator}. Es wird
+ * semiautomatisch durch das Google Plugin erstellt und gepflegt. Daher erfolgt
+ * hier keine weitere Dokumentation. Für weitere Informationen siehe das
+ * synchrone Interface {@link ReportGenerator}.
+ * 
+ * @author thies
+ */
+public interface ReportGeneratorAsync {
+
+}
