9cfb5f9b1aab233bfea5b6568bd623ea18966478
dominik-florschuetz <dominikfloor@googlemail.com>
2017-06-20 13:22:48 +0200

    Tabellen Click Handler angefangen.

--- src/de/worketplace/team06/client/gui/SearchMarketplace.java
+++ src/de/worketplace/team06/client/gui/SearchMarketplace.java
@@ -7,12 +7,18 @@ import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.TextColumn;
 
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SelectionChangeEvent.Handler;
+import com.google.gwt.view.client.SingleSelectionModel;
 
-public class SearchMarketplace {
+public class SearchMarketplace extends Page {
+	
 	private static class Marketplace {
 		private final String title;
 		private final String owner;
 		private final String description;
+		
+		private Marketplace m1 = new Marketplace(title, owner, description);
 		
 		public Marketplace(String title, String owner, String description) {
 			this.title = title;
@@ -26,9 +32,32 @@ public class SearchMarketplace {
 			new Marketplace("Karlsruhe", "Dominik Florschütz", "Hier steht eine Beschreibung des Marktplatzes Karlsruhe"),
 			new Marketplace("Heilbronn", "Tobias Müller", "Hier steht eine Beschreibung des Marktplatzes Heilbronn"));
 	
-	public void load() {
-		final CellTable<Marketplace> table = new CellTable<Marketplace>();
-		table.setPageSize(3);
+	
+	
+	@Override
+	protected String getHeadlineText() {
+		return "<h1>Alle Marktplätze</h1>";
+	}
+	
+	public void run() {
+		
+		final CellTable<Marketplace> allmarketplacestable = new CellTable<Marketplace>();
+		allmarketplacestable.setPageSize(3);
+		
+		final SingleSelectionModel<Marketplace> allmarketplacessm = new SingleSelectionModel<Marketplace>();
+		
+		allmarketplacestable.setSelectionModel(allmarketplacessm);
+		
+		allmarketplacessm.addSelectionChangeHandler(new Handler() { 
+			
+		@Override	
+		public void onSelectionChange (SelectionChangeEvent event) {
+			m1 = allmarketplacessm.getSelectedObject();
+			Page page = new SearchProject(m1);
+			RootPanel.get("Anzeige").clear();
+			RootPanel.get("Anzeige").add(page);
+		}
+	});
 		
 		TextColumn<Marketplace> titleColumn = new TextColumn<Marketplace>() {
 			@Override
@@ -36,7 +65,7 @@ public class SearchMarketplace {
 				return object.title;
 			}
 		};
-		table.addColumn(titleColumn, "Name");
+		allmarketplacestable.addColumn(titleColumn, "Name");
 		
 		TextColumn<Marketplace> ownerColumn = new TextColumn<Marketplace>() {
 			@Override
@@ -44,7 +73,7 @@ public class SearchMarketplace {
 				return object.owner;
 			}
 		};
-		table.addColumn(titleColumn, "Inhaber");
+		allmarketplacestable.addColumn(titleColumn, "Inhaber");
 		
 		TextColumn<Marketplace> descriptionColumn = new TextColumn<Marketplace>() {
 			@Override
@@ -52,8 +81,10 @@ public class SearchMarketplace {
 				return object.description;
 			}
 		};
-		table.addColumn(titleColumn, "Beschreibung");
+		allmarketplacestable.addColumn(titleColumn, "Beschreibung");
+		allmarketplacestable.setRowData(MARKETPLACE);
+		allmarketplacestable.setWidth("100%", true);
 		
-		RootPanel.get("content").add(table);
+		RootPanel.get("content").add(allmarketplacestable);
 	}
 }
