3c8771280625b686194a70979f1630a9441f223d
tobias-mueller1 <me@towbie.de>
2017-06-22 17:21:16 +0200

    Anlegen der Klasse AllCallsMatchingWithUser

---
+++ src/de/worketplace/team06/shared/report/AllCallsMatchingWithUser.java
@@ -0,0 +1,16 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse für alle Ausschreibungen, die dem Partnerprofile des Users entsprechen
+ * @author Toby
+ *
+ */
+public class AllCallsMatchingWithUser extends SimpleReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
