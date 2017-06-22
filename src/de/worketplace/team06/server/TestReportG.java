9b6c0920cfac7e9683922e3d8355dee16bbca120
tobias-mueller1 <me@towbie.de>
2017-06-22 23:32:33 +0200

    createAllCallsReport Methode zum Anzeigen aller Calls in einem Report deklarieren und programmieren. Methoden den entsprechenden Superklassen hinzufügen.

--- src/de/worketplace/team06/server/TestReportG.java
+++ src/de/worketplace/team06/server/TestReportG.java
@@ -2,6 +2,12 @@ package de.worketplace.team06.server;
 
 import de.worketplace.team06.server.report.*;
 
+/**
+ * Diese Klasse dient zum Testen einzelner Methoden für die Erstellung von Reports
+ * und der Ausgabe innerhalb der Console
+ * @author Toby
+ *
+ */
 public class TestReportG {
 
 	public static void main(String[] args) {
--- src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
+++ src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
@@ -56,28 +56,80 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 		return this.wpadmin;
 	}
 	
+	/**
+	 * Methode zum Hinzufügen eines Impressums zum entsprechenden Report
+	 * @param r
+	 */
 	
+	/*
+	 * Impressum hinzufügen (optional)
 	protected void addImprint (Report r){
 		
 		CompositeParagraph imprint = new CompositeParagraph();
 		
-		OrgaUnit orgaUnit = wpadmin.getOrgaUnit();
-        
+		User user = getUser();
+
         switch(orgaUnit.getType()){ 
         case "Person": 
-        	wpadmin.getPersonByGoogleID();
+        	Person p = wpadmin.getPersonByGoogleID(orgaUnit.getGoogleID());
+        	imprint.addSubParagraph(new SimpleParagraph(p.getFirstName() + " " + p.getLastName()));
+        	imprint.addSubParagraph(new SimpleParagraph(p.getStreet()));
+        	imprint.addSubParagraph(new SimpleParagraph(p.getZipcode() + " " + p.getCity()));
         	break;
 		case "Team": 
-			
+			Team t = wpadmin.getTeamByGoogleID(orgaUnit.getGoogleID());
+        	imprint.addSubParagraph(new SimpleParagraph(t.getName()));
             break;
 		case "Organisation": 
-			
+			Organisation o = wpadmin.getOrganisationByGoogleID(orgaUnit.getGoogleID());
+        	imprint.addSubParagraph(new SimpleParagraph(o.getName()));
+        	imprint.addSubParagraph(new SimpleParagraph(o.getStreet()));
+        	imprint.addSubParagraph(new SimpleParagraph(o.getZipcode() + " " + o.getCity()));
         	break;
         }
 		
-		imprint.addSubParagraph(new SimpleParagraph(""));
+		r.setImprint(imprint);
 		
 	}
+	*/
 	
-
+	
+	@Override
+	public AllCallsReport createAllCallsReport(OrgaUnit o) throws IllegalArgumentException {
+		
+		AllCallsReport report = new AllCallsReport();
+		
+		report.setTitle("Alle Ausschreibungen");
+		
+		report.setCreated(new Timestamp(System.currentTimeMillis()));
+		
+		Row headline = new Row();
+		
+		headline.addColumn(new Column("Titel"));
+		headline.addColumn(new Column("Beschreibung"));
+		headline.addColumn(new Column("Projekt"));
+		headline.addColumn(new Column("Deadline"));
+		headline.addColumn(new Column("Status"));
+		
+		report.addRow(headline);
+		
+		
+		Vector<Call> calls = wpadmin.getAllCalls();
+		
+		for (Call c : calls){
+			Project p = wpadmin.getProjectByID(c.getProjectID());
+			Row rowToAdd = new Row();
+			rowToAdd.addColumn(new Column(c.getTitle()));
+			rowToAdd.addColumn(new Column(c.getDescription()));
+			rowToAdd.addColumn(new Column(p.getTitle()));
+			rowToAdd.addColumn(new Column(c.getDeadline().toString()));
+			rowToAdd.addColumn(new Column(c.getStatus()));
+			report.addRow(rowToAdd);
+		}
+		
+		return report;
+	}
+	
+	
+	
 }
