01b7146a317d9f0b85735b81b512a04d0607c839
tobias-mueller1 <me@towbie.de>
2017-06-22 17:20:59 +0200

    Anlegen der Klasse AllCalls

---
+++ src/de/worketplace/team06/shared/report/AllCalls.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse zur Abfrage aller Ausschreibungen
+ * @author Toby
+ *
+ */
+public class AllCalls extends SimpleReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
