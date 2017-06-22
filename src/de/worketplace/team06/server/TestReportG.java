2dce8d2cecd7b076f2f3a5e6113b90176a502f2a
tobias-mueller1 <me@towbie.de>
2017-06-22 23:24:45 +0200

    TestReportG Klasse anlegen

---
+++ src/de/worketplace/team06/server/TestReportG.java
@@ -0,0 +1,12 @@
+package de.worketplace.team06.server;
+
+import de.worketplace.team06.server.report.*;
+
+public class TestReportG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
