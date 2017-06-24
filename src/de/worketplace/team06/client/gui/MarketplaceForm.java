0686db107396de61c58dbb9666ed3c5c92e747e5
levin-roski <lroski@me.com>
2017-06-24 16:21:29 +0200

    OrgaUnitForm anfangen anzupassen.

--- src/de/worketplace/team06/client/gui/MarketplaceForm.java
+++ src/de/worketplace/team06/client/gui/MarketplaceForm.java
@@ -16,10 +16,10 @@ import de.worketplace.team06.shared.WorketplaceAdministrationAsync;
 import de.worketplace.team06.shared.bo.Marketplace;
 
 /**
- * Formular für die Darstellung, Bearbeitung und Löschung eines
- * selektierten Marktplatzes. Falls kein selektierter Marktplatz beim
- * Initialisieren übergeben wird, ist das Formular leer, bereit für die
- * Erstellung eines neuen Marktplatzes.
+ * Formular für die Darstellung, Bearbeitung und Löschung eines selektierten
+ * Marktplatzes. Falls kein selektierter Marktplatz beim Initialisieren
+ * übergeben wird, ist das Formular leer, bereit für die Erstellung eines neuen
+ * Marktplatzes.
  * 
  * @author Roski
  */
@@ -28,9 +28,9 @@ public class MarketplaceForm extends Page {
 			.getWorketplaceAdministration();
 	private Label nameLabel = new Label("Name");
 	private TextBox nameInput = new TextBox();
-	private Label beschreibungLabel = new Label("Beschreibung");
-	private TextBox beschreibungInput = new TextBox();
-	private Boolean shouldUpdate = false;
+	private Label descriptionLabel = new Label("Beschreibung");
+	private TextBox descriptionInput = new TextBox();
+	private boolean shouldUpdate = false;
 	private Marketplace toChangeMarketplace;
 	private HorizontalPanel changeHeadline;
 	private HorizontalPanel addHeadline;
@@ -40,10 +40,12 @@ public class MarketplaceForm extends Page {
 	 * dann bearbeitet und gelöscht werden kann. null übergeben, falls ein neuer
 	 * Marktplatz erstellt werden soll.
 	 * 
-	 * @param pToChangeMarketplace Marketplace, der im Formular angezeigt werden soll
-	 * @param pHeadline Falls true wird dem Formular eine Überschrift vorangehängt
+	 * @param pToChangeMarketplace
+	 *            Marketplace, der im Formular angezeigt werden soll
+	 * @param pHeadline
+	 *            Falls true wird dem Formular eine Überschrift vorangehängt
 	 */
-	public MarketplaceForm (Marketplace pToChangeMarketplace, final boolean pHeadline) {
+	public MarketplaceForm(Marketplace pToChangeMarketplace, final boolean pHeadline) {
 		if (pToChangeMarketplace != null) {
 			shouldUpdate = true;
 			this.toChangeMarketplace = pToChangeMarketplace;
@@ -53,22 +55,26 @@ public class MarketplaceForm extends Page {
 			addHeadline = createHeadline("Marktplatz hinzufügen", true);
 		}
 	}
+
 	/**
 	 * Im Konstruktor kann eine selektierter Marktplatz übergeben werden, der
 	 * dann bearbeitet und gelöscht werden kann. null übergeben, falls ein neuer
 	 * Marktplatz erstellt werden soll.
 	 * 
-	 * @param pToChangeMarketplace Marketplace, der im Formular angezeigt werden soll
-	 * @param pHeadline Falls true wird dem Formular eine Überschrift vorangehängt
-	 * @param pClosingHeadline Falls true wird dem Formular eine Überschrift mit Button, der das aktuelle Item schließt, vorangehängt
+	 * @param pToChangeMarketplace
+	 *            Marketplace, der im Formular angezeigt werden soll
+	 * @param pHeadline
+	 *            Falls true wird dem Formular eine Überschrift vorangehängt
+	 * @param pClosingHeadline
+	 *            Falls true wird dem Formular eine Überschrift mit Button, der
+	 *            das aktuelle Item schließt, vorangehängt
 	 */
-	public MarketplaceForm(Marketplace pToChangeMarketplace, final boolean pHeadline, final Boolean pClosingHeadline) {
+	public MarketplaceForm(Marketplace pToChangeMarketplace, final boolean pHeadline, final boolean pClosingHeadline) {
 		this(pToChangeMarketplace, pHeadline);
 		if (pClosingHeadline) {
 			changeHeadline = createHeadlineWithCloseButton("Marktplatz bearbeiten", true);
 			addHeadline = createHeadlineWithCloseButton("Marktplatz hinzufügen", true);
 		}
-		
 
 		/*
 		 * Grid mit 3 Zeilen und 2 Spalten für das Formular bereitstellen.
@@ -78,29 +84,30 @@ public class MarketplaceForm extends Page {
 		form.setWidth("100%");
 		form.setWidget(0, 0, nameLabel);
 		form.setWidget(0, 1, nameInput);
-		form.setWidget(1, 0, beschreibungLabel);
-		form.setWidget(1, 1, beschreibungInput);
+		form.setWidget(1, 0, descriptionLabel);
+		form.setWidget(1, 1, descriptionInput);
 		final VerticalPanel root = new VerticalPanel();
 		this.add(root);
 		/*
-		 * Falls ein selektierter Marktplatz übergeben wurde und jetzt dargestellt werden soll
+		 * Falls ein selektierter Marktplatz übergeben wurde und jetzt
+		 * dargestellt werden soll
 		 */
 		if (shouldUpdate) {
 			if (changeHeadline != null) {
 				root.add(changeHeadline);
 			}
 			nameInput.setText(toChangeMarketplace.getTitle());
-			beschreibungInput.setText(toChangeMarketplace.getDescription());
+			descriptionInput.setText(toChangeMarketplace.getDescription());
 			final Button saveButton = new Button("Änderungen speichern");
 			saveButton.addClickHandler(new ClickHandler() {
 				public void onClick(ClickEvent event) {
 					if (nameInput.getText().length() == 0) {
 						Window.alert("Bitte vergeben Sie einen Namen");
-					} else if (beschreibungInput.getText().length() == 0) {
+					} else if (descriptionInput.getText().length() == 0) {
 						Window.alert("Bitte beschreiben Sie Ihren Marktplatz genauer");
 					} else {
 						toChangeMarketplace.setTitle(nameInput.getText());
-						toChangeMarketplace.setDescription(beschreibungInput.getText());
+						toChangeMarketplace.setDescription(descriptionInput.getText());
 						worketplaceAdministration.saveMarketplace(toChangeMarketplace, new AsyncCallback<Void>() {
 							public void onFailure(Throwable caught) {
 								Window.alert("Es trat ein Fehler beim Speichern auf, bitte versuchen Sie es erneut");
@@ -143,10 +150,11 @@ public class MarketplaceForm extends Page {
 				public void onClick(ClickEvent event) {
 					if (nameInput.getText().length() == 0) {
 						Window.alert("Bitte vergeben Sie einen Namen");
-					} else if (beschreibungInput.getText().length() == 0) {
+					} else if (descriptionInput.getText().length() == 0) {
 						Window.alert("Bitte beschreiben Sie Ihren Marktplatz genauer");
 					} else {
-						worketplaceAdministration.createMarketplace(nameInput.getText(), beschreibungInput.getText(), new AsyncCallback<Marketplace>() {
+						worketplaceAdministration.createMarketplace(nameInput.getText(), descriptionInput.getText(),
+								new AsyncCallback<Marketplace>() {
 									public void onFailure(Throwable caught) {
 										Window.alert(
 												"Es trat ein Fehler beim Speichern auf, bitte versuchen Sie es erneut");
--- src/de/worketplace/team06/client/gui/OrgaUnitForm.java
+++ src/de/worketplace/team06/client/gui/OrgaUnitForm.java
@@ -1,5 +1,6 @@
 package de.worketplace.team06.client.gui;
 
+import com.google.cloud.sql.jdbc.internal.ClientSideBlob;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
@@ -9,12 +10,14 @@ import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.worketplace.team06.client.ClientsideSettings;
 import de.worketplace.team06.shared.WorketplaceAdministrationAsync;
 import de.worketplace.team06.shared.bo.Marketplace;
+import de.worketplace.team06.shared.bo.OrgaUnit;
 
 /**
  * Formular für die Darstellung und Bearbeitung der aktuellen OrgaUnit. Falls
@@ -28,31 +31,35 @@ import de.worketplace.team06.shared.bo.Marketplace;
 public class OrgaUnitForm extends Page {
 	private WorketplaceAdministrationAsync worketplaceAdministration = ClientsideSettings
 			.getWorketplaceAdministration();
-	private Label nameLabel = new Label("Name");
-	private TextBox nameInput = new TextBox();
-	private Label beschreibungLabel = new Label("Beschreibung");
-	private TextBox beschreibungInput = new TextBox();
-	private Boolean shouldUpdate = false;
-	private Marketplace toChangeMarketplace;
+	private Label typeLabel = new Label("Nutzertyp");
+	private ListBox typeInput = new ListBox();
+	private Label descriptionLabel = new Label("Beschreibung");
+	private TextBox descriptionInput = new TextBox();
+	private boolean shouldUpdate = false;
+	private OrgaUnit toChangeOrgaUnit;
 	private HorizontalPanel changeHeadline;
 	private HorizontalPanel addHeadline;
 
 	/**
-	 * Im Konstruktor kann eine selektierter Marktplatz übergeben werden, der
-	 * dann bearbeitet und gelöscht werden kann. null übergeben, falls ein neuer
-	 * Marktplatz erstellt werden soll.
+	 * Im Konstruktor wird der aktuelle Nutzer .
 	 * 
-	 * 
+	 * @param pHeadline
+	 *            Falls true wird dem Formular eine Überschrift vorangehängt
 	 */
-	public OrgaUnitForm(Marketplace pToChangeMarketplace, final boolean pHeadline) {
-		if (pToChangeMarketplace != null) {
+	public OrgaUnitForm(final boolean pHeadline) {
+		try {
+			toChangeOrgaUnit = ClientsideSettings.getCurrentUser();
 			shouldUpdate = true;
-			this.toChangeMarketplace = pToChangeMarketplace;
+		} catch (Exception e) {
+			shouldUpdate = false;
 		}
 		if (pHeadline) {
-			changeHeadline = createHeadline("Marktplatz bearbeiten", true);
-			addHeadline = createHeadline("Marktplatz hinzufügen", true);
+			changeHeadline = createHeadline("Meinen Nutzer bearbeiten", true);
+			addHeadline = createHeadline("Meinen Nutzer erstellen", true);
 		}
+		typeInput.addItem("Person", "person");
+		typeInput.addItem("Team", "team");
+		typeInput.addItem("Organisation", "organisation");
 	}
 
 	/**
@@ -60,48 +67,46 @@ public class OrgaUnitForm extends Page {
 	 * dann bearbeitet und gelöscht werden kann. null übergeben, falls ein neuer
 	 * Marktplatz erstellt werden soll.
 	 * 
-	 *
+	 * @param pClosingHeadline
+	 *            Falls true wird dem Formular eine Überschrift mit Button, der
+	 *            das aktuelle Item schließt, vorangehängt
 	 */
-	public OrgaUnitForm(Marketplace pToChangeMarketplace, final boolean pHeadline, final Boolean pClosingHeadline) {
-		this(pToChangeMarketplace, pHeadline);
+	public OrgaUnitForm(final boolean pHeadline, final boolean pClosingHeadline) {
+		this(pHeadline);
 		if (pClosingHeadline) {
-			changeHeadline = createHeadlineWithCloseButton("Marktplatz bearbeiten", true);
-			addHeadline = createHeadlineWithCloseButton("Marktplatz hinzufügen", true);
+			changeHeadline = createHeadlineWithCloseButton("Meinen Nutzer bearbeiten", true);
+			addHeadline = createHeadlineWithCloseButton("Meinen Nutzer erstellen", true);
 		}
 
 		/*
-		 * Grid mit 3 Zeilen und 2 Spalten für das Formular bereitstellen.
+		 * Grid mit 8 Zeilen und 2 Spalten für das Formular bereitstellen.
 		 * Danach nötige Panels einfügen und diesem Widget hinzufügen.
 		 */
-		Grid form = new Grid(3, 2);
+		Grid form = new Grid(8, 2);
 		form.setWidth("100%");
-		form.setWidget(0, 0, nameLabel);
-		form.setWidget(0, 1, nameInput);
-		form.setWidget(1, 0, beschreibungLabel);
-		form.setWidget(1, 1, beschreibungInput);
+		form.setWidget(0, 0, typeLabel);
+		form.setWidget(0, 1, typeInput);
+		form.setWidget(1, 0, descriptionLabel);
+		form.setWidget(1, 1, descriptionInput);
 		final VerticalPanel root = new VerticalPanel();
 		this.add(root);
 		/*
-		 * Falls ein selektierter Marktplatz übergeben wurde und jetzt
-		 * dargestellt werden soll
+		 * Falls bereits ein OrgaUnit Objekt existiert und jetzt dargestellt
+		 * werden soll
 		 */
 		if (shouldUpdate) {
 			if (changeHeadline != null) {
 				root.add(changeHeadline);
 			}
-			nameInput.setText(toChangeMarketplace.getTitle());
-			beschreibungInput.setText(toChangeMarketplace.getDescription());
+			descriptionInput.setText(toChangeOrgaUnit.getDescription());
 			final Button saveButton = new Button("Änderungen speichern");
 			saveButton.addClickHandler(new ClickHandler() {
 				public void onClick(ClickEvent event) {
-					if (nameInput.getText().length() == 0) {
-						Window.alert("Bitte vergeben Sie einen Namen");
-					} else if (beschreibungInput.getText().length() == 0) {
-						Window.alert("Bitte beschreiben Sie Ihren Marktplatz genauer");
+					if (descriptionInput.getText().length() == 0) {
+						Window.alert("Bitte beschreiben Sie Ihren Nutzer genauer");
 					} else {
-						toChangeMarketplace.setTitle(nameInput.getText());
-						toChangeMarketplace.setDescription(beschreibungInput.getText());
-						worketplaceAdministration.saveMarketplace(toChangeMarketplace, new AsyncCallback<Void>() {
+						toChangeOrgaUnit.setDescription(descriptionInput.getText());
+						worketplaceAdministration.savePerson(toChangeOrgaUnit, new AsyncCallback<Void>() {
 							public void onFailure(Throwable caught) {
 								Window.alert("Es trat ein Fehler beim Speichern auf, bitte versuchen Sie es erneut");
 							}
@@ -114,13 +119,21 @@ public class OrgaUnitForm extends Page {
 				}
 			});
 			final VerticalPanel panel = new VerticalPanel();
+			if (toChangeOrgaUnit.getType() == "person") {
+				// TODO
+
+			} else if (toChangeOrgaUnit.getType() == "team") {
+				// TODO
+			} else {
+				// TODO
+			}
 			panel.add(saveButton);
 			final Button deleteButton = new Button("Diesen Marktplatz entfernen");
 			deleteButton.addClickHandler(new ClickHandler() {
 				public void onClick(ClickEvent event) {
 					final boolean confirmDelete = Window.confirm("Möchten Sie den Marktplatz wirklich löschen?");
 					if (confirmDelete) {
-						worketplaceAdministration.deleteMarketplace(toChangeMarketplace, new AsyncCallback<Void>() {
+						worketplaceAdministration.deleteMarketplace(toChangeOrgaUnit, new AsyncCallback<Void>() {
 							public void onFailure(Throwable caught) {
 								Window.alert("Es trat ein Fehler beim Löschen auf, bitte versuchen Sie es erneut");
 							}
@@ -143,10 +156,10 @@ public class OrgaUnitForm extends Page {
 				public void onClick(ClickEvent event) {
 					if (nameInput.getText().length() == 0) {
 						Window.alert("Bitte vergeben Sie einen Namen");
-					} else if (beschreibungInput.getText().length() == 0) {
+					} else if (descriptionInput.getText().length() == 0) {
 						Window.alert("Bitte beschreiben Sie Ihren Marktplatz genauer");
 					} else {
-						worketplaceAdministration.createMarketplace(nameInput.getText(), beschreibungInput.getText(),
+						worketplaceAdministration.createMarketplace(nameInput.getText(), descriptionInput.getText(),
 								new AsyncCallback<Marketplace>() {
 									public void onFailure(Throwable caught) {
 										Window.alert(
