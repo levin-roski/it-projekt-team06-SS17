6ad24a93d045681d6867b4198ced424355ad33ad
tobias-mueller1 <me@towbie.de>
2017-06-27 16:19:54 +0200

    AllCallsOfUser-Klasse anlegen

---
+++ src/de/worketplace/team06/shared/report/AllCallsOfUserReport.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse zur Abfrage aller Ausschreibungen für einen User
+ * @author Toby
+ *
+ */
+public class AllCallsOfUserReport extends SimpleReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
