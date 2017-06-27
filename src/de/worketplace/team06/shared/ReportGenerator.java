98f4ab6af828eded83b5e4227938067a57363364
tobias-mueller1 <me@towbie.de>
2017-06-27 17:54:01 +0200

    Methoden für Erstellung von Reports für Verflechtungen deklarieren

--- src/de/worketplace/team06/shared/ReportGenerator.java
+++ src/de/worketplace/team06/shared/ReportGenerator.java
@@ -8,8 +8,12 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import de.worketplace.team06.shared.bo.*;
 import de.worketplace.team06.shared.report.AllApplicationsForCallsOfUserReport;
+import de.worketplace.team06.shared.report.AllApplicationsOfApplicantReport;
+import de.worketplace.team06.shared.report.AllApplicationsOfUserToCallsReport;
 import de.worketplace.team06.shared.report.AllCallsMatchingWithUserReport;
 import de.worketplace.team06.shared.report.AllCallsReport;
+import de.worketplace.team06.shared.report.AllEnrollmentsOfApplicantReport;
+import de.worketplace.team06.shared.report.AllInterrelationsOfApplicantReport;
 
 /**
  * @author Toby
@@ -26,8 +30,19 @@ public interface ReportGenerator extends RemoteService {
 
 	public AllApplicationsForCallsOfUserReport createAllApplicationsForCallsOfUserReport(OrgaUnit o)
 			throws IllegalArgumentException;
+	
+	public AllApplicationsOfUserToCallsReport createAllApplicationsOfUserToCallsReport(OrgaUnit o)
+			throws IllegalArgumentException;
 
+	public AllApplicationsOfApplicantReport createAllApplicationsOfApplicantReport(OrgaUnit applicant) throws IllegalArgumentException;
+
+	public AllEnrollmentsOfApplicantReport createAllEnrollmentsOfApplicantReport(OrgaUnit applicant) throws IllegalArgumentException;
+
+	public AllInterrelationsOfApplicantReport createAllInterrelationsOfApplicantReport(OrgaUnit applicant)
+			throws IllegalArgumentException;
+	
 	public OrgaUnit getOrgaUnitFor(LoginInfo loginInfo) throws IllegalArgumentException;
 
 
+	
 }
