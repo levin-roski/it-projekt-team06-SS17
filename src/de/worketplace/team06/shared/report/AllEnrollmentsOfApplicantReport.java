cf1fecfdf46664bea5375324f113d66a45c44714
tobias-mueller1 <me@towbie.de>
2017-06-27 17:17:45 +0200

    Report-Klasse für Teilnahmen eines Bewerbers erstellen

---
+++ src/de/worketplace/team06/shared/report/AllEnrollmentsOfApplicantReport.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse zur Abfrage aller Teilnahmen eines Bewerbers
+ * @author Toby
+ *
+ */
+public class AllEnrollmentsOfApplicantReport extends SimpleReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
