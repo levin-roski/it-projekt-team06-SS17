758e20f2da860fb9a9fa1ffcec14460043470f42
tobias-mueller1 <me@towbie.de>
2017-06-27 17:54:33 +0200

    Testmethoden zur Anzeige von Reports erweitern

--- src/de/worketplace/team06/server/TestReportG.java
+++ src/de/worketplace/team06/server/TestReportG.java
@@ -1,7 +1,7 @@
 package de.worketplace.team06.server;
 
 import de.worketplace.team06.server.report.*;
-import de.worketplace.team06.shared.bo.Person;
+import de.worketplace.team06.shared.bo.*;
 import de.worketplace.team06.shared.report.*;
 
 /**
@@ -23,13 +23,26 @@ public class TestReportG {
 	}
 	
 	public static void ShowReport(ReportGeneratorImpl gen){
-		Person p = new Person();
-		p.setGoogleID("G3000");
+		LoginInfo login = new LoginInfo();
+		login.setGoogleId("G3000");
+		login.setEmailAddress("test@mail.de");
+		login.setNickname("Testuser");
+		
+		OrgaUnit o = gen.getOrgaUnitFor(login);
+		
+		if (o instanceof Person){
+			Person p = (Person) o;
+		} else if (o instanceof Team){
+			Team t = (Team) o;
+		} else if (o instanceof Organisation){
+			Organisation org = (Organisation) o;
+		}
+		
 		PlainTextReportWriter writer = new PlainTextReportWriter();
 		//HTMLReportWriter writer = new HTMLReportWriter();
-		writer.process(gen.createAllCallsReport());
-		writer.process(gen.createAllCallsOfUserReport(p));
-		//writer.process(gen.createAllApplicationsForCallsOfUserReport(p));
+		//writer.process(gen.createAllCallsReport());
+		//writer.process(gen.createAllCallsOfUserReport(testuser));
+		writer.process(gen.createAllApplicationsForCallsOfUserReport(o));
 		System.out.println(writer.getReportText());
 	}
 
