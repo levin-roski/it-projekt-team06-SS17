5da10e80d670a06fc120098afa92a8d22266f882
tobias-mueller1 <me@towbie.de>
2017-06-27 17:52:31 +0200

    ReportKlasse für alle Verflechtungen des Bewerbers angelegt

---
+++ src/de/worketplace/team06/shared/report/AllInterrelationsOfApplicantReport.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse zur Abfrage aller Verflechtungen eines Bewerbers
+ * @author Toby
+ *
+ */
+public class AllInterrelationsOfApplicantReport extends CompositeReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
