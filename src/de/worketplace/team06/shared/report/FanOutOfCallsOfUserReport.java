0f0ec3f55223fd20d53a076069f623da01eb7bda
tobias-mueller1 <me@towbie.de>
2017-06-29 21:17:43 +0200

    Änderung der Namen

---
+++ src/de/worketplace/team06/shared/report/FanOutOfCallsOfUserReport.java
@@ -0,0 +1,17 @@
+package de.worketplace.team06.shared.report;
+
+/**
+ * Reportklasse zur FanOut Analyse aller Bewerbungen des Users
+ * @author Toby
+ *
+ */
+public class FanOutOfCallsOfUserReport extends SimpleReport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
