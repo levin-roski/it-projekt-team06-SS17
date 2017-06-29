892640bf6368f1e4449b804d13b452f1a6a0c214
tobias-mueller1 <me@towbie.de>
2017-06-29 19:44:47 +0200

    CompositeReports in den HTML Writer integrieren

--- src/de/worketplace/team06/shared/report/HTMLReportWriter.java
+++ src/de/worketplace/team06/shared/report/HTMLReportWriter.java
@@ -95,61 +95,144 @@ public class HTMLReportWriter extends ReportWriter {
 	
 	@Override
 	public void process(AllCallsOfUserReport r) {
-		// TODO Auto-generated method stub
+		simpleReportProcess(r);
 		
 	}
 	
 	@Override
 	public void process(AllCallsMatchingWithUserReport r) {
-		// TODO Auto-generated method stub
+		simpleReportProcess(r);
 		
 	}
 	
 	@Override
 	public void process(AllApplicationsForCallsOfUserReport r) {
-		// TODO Auto-generated method stub
+		simpleReportProcess(r);
 		
 	}
 	
 	@Override
 	public void process(AllApplicationsOfUserToCallsReport r) {
-		// TODO Auto-generated method stub
+		simpleReportProcess(r);
 		
-	}
-  
-	/**
-	 * Auslesen des Ergebnisses der zuletzt aufgerufenen Prozessierungsmethode.
-	 * 
-	 * @return ein String im HTML-Format
-	 */
-	public String getReportText() {
-	  return this.reportText;
 	}
 
 	@Override
 	public void process(AllApplicationsOfApplicantReport r) {
-		// TODO Auto-generated method stub
+		this.resetReportText();
+		
+		StringBuffer result = new StringBuffer();
+		
+		result.append("<H2>" + r.getTitle() + "</H2>");
+		result.append("<table><tr>");
+
+		Vector<Row> rows = r.getRows();
+		result.append("<table>");
+		for (int i = 0; i < rows.size(); i++) {
+			Row row = rows.elementAt(i);
+		    result.append("<tr>");
+		    for (int j = 0; j < row.getNumColumns(); j++) {
+		    	result.append("<td>" + row.getColumnAt(j) + "</td>");
+		    }
+		    result.append("</tr>");
+		}
+		
+		result.append("</table>");
+		this.reportText = result.toString();
 		
 	}
 
 	@Override
 	public void process(AllEnrollmentsOfApplicantReport r) {
-		// TODO Auto-generated method stub
+		this.resetReportText();
+		
+		StringBuffer result = new StringBuffer();
+		
+		result.append("<H2>" + r.getTitle() + "</H2>");
+		result.append("<table><tr>");
+
+		Vector<Row> rows = r.getRows();
+		result.append("<table>");
+		for (int i = 0; i < rows.size(); i++) {
+			Row row = rows.elementAt(i);
+		    result.append("<tr>");
+		    for (int j = 0; j < row.getNumColumns(); j++) {
+		    	result.append("<td>" + row.getColumnAt(j) + "</td>");
+		    }
+		    result.append("</tr>");
+		}
+		
+		result.append("</table>");
+		this.reportText = result.toString();
 		
 	}
 
 	@Override
 	public void process(AllInterrelationsOfApplicantReport r) {
-		// TODO Auto-generated method stub
+		this.resetReportText();
+		
+		StringBuffer result = new StringBuffer();
+		
+		result.append("<H1>" + r.getTitle() + "</H1>");
+		result.append("<table><tr>");
+		if (r.getHeaderData() != null){
+			result.append("<td>" + paragraph2HTML(r.getHeaderData()) + "</td></tr>");
+		}
+		//result.append("<td valign=\"top\">" + paragraph2HTML(r.getImprint()) + "</td>");
+		result.append("<tr><td>" + r.getCreated().toString() + "</td></tr></table>");
+
+		for (int i = 0; i < r.getNumSubReports(); i++) {
+
+			if (r.getSubReportAt(i) instanceof AllApplicationsOfApplicantReport){
+				AllApplicationsOfApplicantReport subReport = (AllApplicationsOfApplicantReport) r.getSubReportAt(i);
+				this.process(subReport);
+				
+			} else if (r.getSubReportAt(i) instanceof AllEnrollmentsOfApplicantReport){
+				AllEnrollmentsOfApplicantReport subReport = (AllEnrollmentsOfApplicantReport) r.getSubReportAt(i);
+				this.process(subReport);
+			}
+			
+		    result.append(this.reportText + "\n");
+
+		    this.resetReportText();
+		}
+		this.reportText = result.toString();
 		
 	}
 
 	@Override
 	public void process(AllInterrelationsOfAllApplicantsOfUserReport r) {
-		// TODO Auto-generated method stub
+		this.resetReportText();
 		
+		StringBuffer result = new StringBuffer();
+		
+		result.append("<H1>" + r.getTitle() + "</H1>");
+		result.append("<table><tr>");
+		if (r.getHeaderData() != null){
+			result.append("<td>" + paragraph2HTML(r.getHeaderData()) + "</td></tr>");
+		}
+		//result.append("<td valign=\"top\">" + paragraph2HTML(r.getImprint()) + "</td>");
+		result.append("<tr><td>" + r.getCreated().toString() + "</td></tr></table>");
+
+		for (int i = 0; i < r.getNumSubReports(); i++) {
+
+			AllInterrelationsOfApplicantReport subReport = (AllInterrelationsOfApplicantReport) r.getSubReportAt(i);
+			this.process(subReport);
+				
+		    result.append(this.reportText + "\n");
+
+		    this.resetReportText();
+		}
+		this.reportText = result.toString();
+
 	}
 	
+	/**
+	 * Einheitliche Methode zum Erstellen von SimpleReports in HTML-Format.
+	 * Da die Logik der Erstellung von SimpleReports und der Aufbaue identisch ist,
+	 * genügt jeweils der Aufruf dieser Methode.
+	 * @param r
+	 */
 	private void simpleReportProcess(SimpleReport r){
 		this.resetReportText();
 		
@@ -176,5 +259,14 @@ public class HTMLReportWriter extends ReportWriter {
 		
 		result.append("</table>");
 		this.reportText = result.toString();
+	}
+	
+	/**
+	 * Auslesen des Ergebnisses der zuletzt aufgerufenen Prozessierungsmethode.
+	 * 
+	 * @return ein String im HTML-Format
+	 */
+	public String getReportText() {
+	  return this.reportText;
 	}
 }
