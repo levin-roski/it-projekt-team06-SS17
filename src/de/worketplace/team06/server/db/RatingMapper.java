a9d31ca873495fc16b3cf8c93f9e579583e77a9f
theresa-vocke <theresa.vocke@outlook.de>
2017-06-30 17:45:25 +0200

    Imports RatingMapper korrigieren.

--- src/de/worketplace/team06/server/db/RatingMapper.java
+++ src/de/worketplace/team06/server/db/RatingMapper.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
+import java.util.Vector;
 
 import de.worketplace.team06.shared.bo.Rating;
 /**
