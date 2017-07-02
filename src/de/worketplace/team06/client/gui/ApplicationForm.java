f3b294b10ccc53666a40bf3fa0687ca6b672874d
johannes-mueller1 <jm097@hdm-stuttgart.de>
2017-07-02 21:14:28 +0200

    ApplicationForm anpassen.

--- src/de/worketplace/team06/client/gui/ApplicationForm.java
+++ src/de/worketplace/team06/client/gui/ApplicationForm.java
@@ -200,6 +200,12 @@ public class ApplicationForm extends Form {
 				new AsyncCallback<Rating>() {
 					@Override
 					public void onFailure(Throwable caught) {
+						ratingHeadline.setVisible(true);
+						ratingLabel.setText("Noch keine Bewertung vergeben");
+						ratingLabel.setVisible(true);
+						ratingInput.setVisible(false);
+						ratingTextLabel.setVisible(false);
+						ratingTextInput.setVisible(false);
 					}
 
 					@Override
@@ -217,7 +223,16 @@ public class ApplicationForm extends Form {
 						ratingTextInput.setEnabled(false);
 						ratingHeadline.setVisible(true);
 						}
+						else{
+							ratingHeadline.setVisible(true);
+							ratingLabel.setText("Noch keine Bewertung vergeben");
+							ratingLabel.setVisible(true);
+							ratingInput.setVisible(false);
+							ratingTextLabel.setVisible(false);
+							ratingTextInput.setVisible(false);
+						}
 					}
+					
 				});
 	}
 }
