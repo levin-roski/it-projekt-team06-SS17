08adfb4fe2431a76b986efee01fc10b90167775d
patrick-strepp <ps108@hdm-stuttgart.de>
2017-07-02 15:30:35 +0200

    CreateallCallsMatchingWithUserReport Methode erweitern

--- src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
+++ src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
@@ -233,12 +233,8 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 		report.setTitle("Alle interessanten Ausschreibungen für den Benutzer");
 		report.setCreated(new Date());
 		
-		//Generierung der Kopfdaten des Reports
-		CompositeParagraph headerData = new CompositeParagraph();
-		headerData.addSubParagraph(new SimpleParagraph("User: " + getNameForOrgaUnit(o)));
-		headerData.addSubParagraph(new SimpleParagraph("Datum: " + getDateForReport(report)));
-		headerData.addSubParagraph(new SimpleParagraph("Uhrzeit: " + getTimeForReport(report)));
-		report.setHeaderData(headerData);
+		//Hinzufügen der Kopfdaten
+		report.setHeaderData(createHeaderData(o, report));
 		
 		Row headline = new Row();
 		
