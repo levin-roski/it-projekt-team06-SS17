2a5561dca91180e43adb9a2133a0857ba0022777
patrick-strepp <ps108@hdm-stuttgart.de>
2017-07-02 15:33:50 +0200

    createFanOutOfCallsOfUserReport Methode erweitern

--- src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
+++ src/de/worketplace/team06/server/report/ReportGeneratorImpl.java
@@ -629,12 +629,8 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 		report.setTitle("FanOut: Anzahl der Ausschreibungen");
 		report.setCreated(new Date());
 		
-		//Generierung der Kopfdaten des Reports
-		CompositeParagraph headerData = new CompositeParagraph();
-		headerData.addSubParagraph(new SimpleParagraph("User: " + getNameForOrgaUnit(o)));
-		headerData.addSubParagraph(new SimpleParagraph("Datum: " + getDateForReport(report)));
-		headerData.addSubParagraph(new SimpleParagraph("Uhrzeit: " + getTimeForReport(report)));
-		report.setHeaderData(headerData);
+		//Hinzufügen der Kopfdaten
+		report.setHeaderData(createHeaderData(o, report));
 		
 		Row headline = new Row();
 		
